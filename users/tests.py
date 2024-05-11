613eb101ae0b2b3b12928f5432cd991eac797938
vergelsky <d.vergelsky@gmail.com>
2024-05-11 21:49:57 +0700

    Создал и протестил пользователей

---
+++ users/tests.py
@@ -0,0 +1,59 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from users.models import User
+
+
+class UserTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            email="test@test.tst",
+            password="testpassword"
+        )
+        self.data = {
+            "email": "test1@test.tst",
+            "password": "testpassword"
+        }
+        self.upd_data = {
+            "email": "test2@test.tst"
+        }
+
+    def test_create_user(self):
+        """Тестируем создание пользователя"""
+
+        response = self.client.post(reverse("users:user-list"), data=self.data)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+    def test_read_users(self):
+        """ Тестируем чтение списка пользователей"""
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(reverse('users:user-list'))
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_update_user(self):
+        """ Тестируем обновление пользователя"""
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.patch(reverse('users:user-detail', args=[self.user.pk]), data=self.upd_data)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_delete_user(self):
+        """ Тестируем удаление пользователя """
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(reverse('users:user-detail', args=[self.user.pk]))
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
--- users/urls.py
+++ users/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from users.apps import UsersConfig
+from users.views import UserViewSet
+
+app_name = UsersConfig.name
+
+router = DefaultRouter()
+
+router.register(r'', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-]
+                  path('admin/', admin.site.urls),
+              ] + router.urls
