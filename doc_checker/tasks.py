265e08575b0e94636b32b794fc4bb9bdf5cf976d
Vergelsky <graf.werger@gmail.com>
2024-05-22 01:22:23 +0700

    Переделал отправку письма модератору через очередь

--- doc_checker/tasks.py
+++ doc_checker/tasks.py
@@ -1,7 +1,12 @@
 from celery import shared_task
-from doc_checker.services import send_document_verification_results
+from doc_checker.services import send_document_verification_results, send_user_docs_for_check
 
 
 @shared_task
 def task_send_document_verification_results(email, result):
     send_document_verification_results(email, result)
+
+
+@shared_task
+def task_send_user_docs_for_check(doc):
+    send_user_docs_for_check(doc)
--- doc_checker/views.py
+++ doc_checker/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 
 from doc_checker.models import Document
 from doc_checker.serializers import DocumentSerializer
-from doc_checker.services import send_user_docs_for_check
+from doc_checker.tasks import task_send_user_docs_for_check
 
 
 class DocumentViewSet(viewsets.ModelViewSet):
@@ -11,5 +11,5 @@ class DocumentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-        send_user_docs_for_check(serializer.instance)
+        task_send_user_docs_for_check.delay(serializer.instance)
         return serializer
